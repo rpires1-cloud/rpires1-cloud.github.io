9988b7b40f319b1f1046423ea002b19949af2e93
rpires1-cloud <r.pires1@edu.ulisboa.pt>
2026-06-12 14:32:48 +0100

    JS implementation and other adjustments - june 12

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,1 +1,13 @@
-//alert("isto é feito com js");
+let inactiveThumbnails = document.getElementsByClassName('product')
+let activeThumbnails = document.getElementsByClassName('active')
+
+for (var i=0; i<inactiveThumbnails.length; i++) {
+    inactiveThumbnails[i].addEventListener('click', function(){
+        if (activeThumbnails.length > 0 ){
+            activeThumbnails[0].classList.remove('active')
+        }
+
+        this.classList.add('active')
+        document.getElementById('selected').src = this.src
+    })
+}
